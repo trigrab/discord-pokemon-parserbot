11f3513d0bc5f3563ff3a265fa856ba650210b96
Lukas Veneziano <fs@venezilu.de>
2018-03-20 19:43:20 +0100

    fix rest_client

--- PokemonParser/REST_Client.py
+++ PokemonParser/REST_Client.py
@@ -14,14 +14,14 @@ class RESTClient:
         self.auth_header = {'Authorization': 'Token ' + api_token}
 
     def post_pokemon(self, pokemon: PokemonSpawn):
-        post_data = json.dumps({
+        post_data = {
             'poke_nr': pokemon.number,
             'poke_lat': pokemon.latitude,
             'poke_lon': pokemon.longitude,
             "poke_iv": pokemon.iv,
             "poke_lvl": pokemon.lvl,
             "poke_despawn_time": pokemon.despawn_time
-        })
+        }
         logger.info(post_data)
         return requests.post(self.api_url, json=post_data, headers=self.auth_header)
 
