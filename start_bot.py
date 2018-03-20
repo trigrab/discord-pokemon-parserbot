d8e056bdf6c661173ed5b28fc5c5ff8694116169
Lukas Veneziano <fs@venezilu.de>
2018-03-20 18:34:14 +0100

    make logging less verbose

--- start_bot.py
+++ start_bot.py
@@ -2,13 +2,13 @@ from PokemonParser.Bot import Bot
 import logging
 
 logFormatter = logging.Formatter(
-    "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+    "%(asctime)s [%(levelname)-5.5s]  %(message)s")
 LOGGER1 = logging.getLogger('discord')
 LOGGER2 = logging.getLogger('mybot')
 consoleHandler = logging.StreamHandler()
 consoleHandler.setFormatter(logFormatter)
 LOGGER1.addHandler(consoleHandler)
-LOGGER1.setLevel(level=logging.INFO)
+LOGGER1.setLevel(level=logging.WARNING)
 LOGGER2.addHandler(consoleHandler)
 LOGGER2.setLevel(level=logging.DEBUG)
 
